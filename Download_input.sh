4c96a78399ce5f54c1a81a257c69612fa3587b07
Donghang Lu <lu562@purdue.edu>
2018-11-15 15:16:36 -0500

    2

--- Download_input.sh
+++ Download_input.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+pids=""
+RESULT=0
 while IFS='' read -r line ; do
     curl -sSO $line &
+    pids="$pids $!"
 done < "$1"
+
+for pid in $pids; do
+    wait $pid || let "RESULT=1"
+done
+
+if [ "$RESULT" == "1" ];
+    then
+       exit 1
+fi
